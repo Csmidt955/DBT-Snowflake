aea9a93dbaeafe21fdf8a0cf79acc63f5a90cf60
cooper.smidt@mammothgrowth.com <cooper.smidt@mammothgrowth.com>
2022-02-19 17:53:03 +0000

    initialize

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -3,11 +3,17 @@ with payments as (
     select
         id as payment_id,
         orderid as order_id,
+        paymentmethod as payment_method,
         status,
-        amount/100 as amount
+
+        -- amount is stored in cents, convert it to dollars
+        {{ cents_to_dollars('amount') }} as amount,
+        created as created_at
 
     from {{ source('stripe', 'payment')}}
 
 )
 
 select * from payments
+
+
